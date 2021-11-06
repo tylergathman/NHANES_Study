17d64166d22b0b6524e0d6f673145e5977fca93e
tylergathman <66639466+tylergathman@users.noreply.github.com>
2021-11-06 16:21:21 -0500

    Delete NHANES_PreProcess.py

--- NHANES_PreProcess.py
+++
@@ -1,8 +0,0 @@
-import pyreadstat
-# Read the sas7bdat file
-
-# Written by TJG on 11/5/21
-df, meta = pyreadstat.read_xport('DEMO.xpt')
-type(df)
-df.head()
-df.to_csv('data_from_sas.csv', index=False)
